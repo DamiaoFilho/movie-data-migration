8f2cdfc63504159bb0179f7948962922965dddc5
DamiaoFilho <damiaofilho0123@gmail.com>
2024-08-11 22:23:46 -0300

    fixed some bugs on upload

--- movie_migrations/views.py
+++ movie_migrations/views.py
@@ -4,7 +4,8 @@ from .forms import MigrationForm
 import csv
 from movies.models import Movie
 from ratings.models import Rating
-from tags.models import Tag
+from links.models import Link
+from tags.models import Tag, GenomeScore, GenomeTag
 import datetime
 import pytz
 import time
@@ -12,12 +13,38 @@ from django.db import connection
 
 class CSVHandlerMixIn:
     
-    def handle_movie_file(self, file, model):
+    def handle_movie_file(self, file, model, columns):
         start = time.time( )
         decoded_file = file.read().decode('utf-8').splitlines()
+        
+        batch_size = 10000 
+        rows = []
+
+        reader = csv.DictReader(decoded_file)
+        for row in reader:
+            values = list(row.values())
+            rows.append(values)
+            
+            if len(rows) >= batch_size:
+                print(f'Inserindo registros')
+                self.bulk_insert(model, rows, columns)
+                rows = []  
+        
+        if rows:
+            self.bulk_insert(model, rows, columns)
+    
+        end = time.time()
+        print(end-start)
+        return end-start
+    
+    def handle_movie_file_2(self, file, model):
+        start = time.time( )
+        decoded_file = file.read().decode('utf-8').splitlines()
+        
         reader = csv.DictReader(decoded_file)
         for row in reader:
             model.objects.create(**row)
+            
         end = time.time()
         print(end-start)
         return end-start
@@ -25,7 +52,7 @@ class CSVHandlerMixIn:
     def handle_movie_depedent_file(self, file, model, columns):
         start = time.time()
         
-        movies_dict = {movie.movieId: movie.movieId for movie in Movie.objects.all()}
+        movies_dict = {movie.id: movie.id for movie in Movie.objects.all()}
         
         decoded_file = file.read().decode('utf-8').splitlines()
         reader = csv.DictReader(decoded_file)
@@ -35,21 +62,19 @@ class CSVHandlerMixIn:
 
         print('Iniciando a leitura do arquivo')
         for row in reader:
-            movieId = int(row.pop('movieId'))
-            timestamp = row.pop('timestamp')
+            movieId = int(row.get('movieId'))
+            timestamp = None
+            if 'timestamp' in row.keys():
+                timestamp = row.pop('timestamp')
             
             if timestamp:
                 timestamp = datetime.datetime.fromtimestamp(int(timestamp))
                 timestamp = pytz.utc.localize(timestamp)
             
             if movieId in movies_dict.keys():
-                movie_db_id = movies_dict[movieId]
-                values = [
-                    int(row.get('userId')),            
-                    float(row.get('rating')),           
-                    timestamp.strftime('%Y-%m-%d %H:%M:%S') if timestamp else None,
-                    movie_db_id,                 
-                ]
+                values = list(row.values())
+                if timestamp:
+                    values.append(timestamp)    
                 rows.append(values)
             
             if len(rows) >= batch_size:
@@ -61,7 +86,6 @@ class CSVHandlerMixIn:
             self.bulk_insert(model, rows, columns)
         
         end = time.time()
-        print(end-start)
         return end-start
     
     def bulk_insert(self, model, rows, columns):
@@ -74,18 +98,27 @@ class CSVHandlerMixIn:
         with connection.cursor() as cursor:
             cursor.executemany(sql % placeholders, rows)
                 
-    def handle_tag_depedent_file(self, file, model):
+    def handle_tag_depedent_file(self, file, model, columns):
         start = time.time()
         decoded_file = file.read().decode('utf-8').splitlines()
+        
+        tags_dict = {tag.id: tag.id for tag in Tag.objects.all()}
+        batch_size = 10000 
+        rows = []
+        
         reader = csv.DictReader(decoded_file)
         for row in reader:
-            tagId = row.pop('tagId')
-            if tagId:
-                tagId = Tag.objects.filter(tagId=tagId).first()
-                model.objects.create(tagId=tagId, **row)
+            tagId = int(row.get('tagId'))
+            if tagId in tags_dict.keys():
+                values = list(row.values())
+                rows.append(values)
+                
+        if len(rows) >= batch_size:
+            print(f'Inserindo registros')
+            self.bulk_insert(model, rows, columns)
+            rows = []  
                 
         end = time.time()
-        print(end-start)
         return end-start
 
 class MigrationView(FormView, CSVHandlerMixIn):
@@ -94,11 +127,17 @@ class MigrationView(FormView, CSVHandlerMixIn):
     success_url = "/"
     
     def form_valid(self, form):
-        self.handle_movie_depedent_file(form.cleaned_data['ratings'], Rating, ['user_id', 'rating', 'timestamp', 'movie_id'])
-        # self.handle_movie_depedent_file(form.cleaned_data['links'], Link)
-        # self.handle_movie_depedent_file(form.cleaned_data['tags'], Tag)
-        # self.handle_csv_file(form.cleaned_data['genome_tags'], GenomeTag)
-            # self.handle_csv_file(form.cleaned_data['genome_scores'], GenomeScore)
-        # self.handle_movie_file(form.cleaned_data['movies'], Movie)
+        if form.cleaned_data['movies']:
+            self.handle_movie_file(form.cleaned_data['movies'], Movie, ['id', 'title', 'genres'])
+        if form.cleaned_data['ratings']:
+            self.handle_movie_depedent_file(form.cleaned_data['ratings'], Rating, ['user_id', 'movie_id', 'rating', 'timestamp'])
+        if form.cleaned_data['links']:
+            self.handle_movie_depedent_file(form.cleaned_data['links'], Link, ['movie_id', 'imdb_id', 'tmdb_id'])
+        if form.cleaned_data['tags']:
+            self.handle_movie_depedent_file(form.cleaned_data['tags'], Tag, ['user_id', 'movie_id', 'tag', 'timestamp'])
+        if form.cleaned_data['genome_tags']:
+            self.handle_tag_depedent_file(form.cleaned_data['genome_tags'], GenomeTag, ['tag_id', 'tag_details'])
+        if form.cleaned_data['genome_scores']:
+            self.handle_tag_depedent_file(form.cleaned_data['genome_scores'], GenomeScore,  ['movie_id', 'tag_id', 'relevance'])
         
         return super().form_valid(form)
